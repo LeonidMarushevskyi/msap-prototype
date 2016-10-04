0d48abeb13088e92d6a5878064c35863c83741c2
Alexander Serbin <alexander.serbin@engagepoint.com>
2016-10-04 15:43:20 -0400

    MSAP-65 added Open Slots and Special Needs to details

--- src/main/java/com/engagepoint/msap/domain/LookupSpecialNeedType.java
+++ src/main/java/com/engagepoint/msap/domain/LookupSpecialNeedType.java
@@ -1,14 +1,21 @@
 package com.engagepoint.msap.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.Objects;
 
 /**
@@ -27,10 +34,11 @@ public class LookupSpecialNeedType implements Serializable {
     @NotNull
     @Column(name = "name", nullable = false)
     private String name;
-    
+
     @OneToOne
     private LookupSpecialNeedGroup specialNeedGroup;
 
+    @JsonBackReference("specialNeeds")
     @ManyToOne
     @JoinColumn(name = "provider_id")
     private Provider provider;
@@ -46,7 +54,7 @@ public class LookupSpecialNeedType implements Serializable {
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
