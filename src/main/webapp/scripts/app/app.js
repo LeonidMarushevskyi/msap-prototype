b779a1c646ab1831ed3067bae0383628af8cd6a2
oleg.korneychuk <oleg.korneychuk@engagepoint.com>
2016-10-06 13:35:40 -0400

    Fix return to login page

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -54,7 +54,7 @@ angular.module('msapApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalpre
             // reset the state memory after logout. If we're redirected to login, our
             // previousState is already set in the authExpiredInterceptor. If we're going
             // to login directly, we don't want to be sent to some previous state anyway
-            if (toState.name !== 'login' && $rootScope.previousStateName) {
+            if (toState.name !== 'home' && $rootScope.previousStateName) {
               $rootScope.previousStateName = fromState.name;
               $rootScope.previousStateParams = fromParams;
             }
